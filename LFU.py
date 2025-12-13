0244d949aa70236a7acf991ae71990c23e6ee05b
Sneha M S <snehams033@gmail.com>
2025-12-14 00:51:39 +0530

    Update LFU cache logic

--- LFU.py
+++ LFU.py
@@ -1,11 +1,11 @@
 def lfu_page_replacement():
-    # User inputs
+
     memory_size = int(input("Enter memory size: "))
     cache_size = int(input("Enter cache (frame) size: "))
 
     reference_string = list(map(int, input("Enter page reference string (space-separated): ").split()))
 
-    # Data structures
+   
     cache = []
     frequency = {}
     page_faults = 0
@@ -15,27 +15,26 @@ def lfu_page_replacement():
     for page in reference_string:
         fault = False
 
-        # If page is not in cache → page fault
         if page not in cache:
             page_faults += 1
             fault = True
 
-            # If cache has space
+            
             if len(cache) < cache_size:
                 cache.append(page)
                 frequency[page] = 1
             else:
-                # Find LFU page
+                
                 lfu_page = min(cache, key=lambda x: frequency[x])
                 
-                # Replace LFU page
+              
                 cache.remove(lfu_page)
                 del frequency[lfu_page]
 
                 cache.append(page)
                 frequency[page] = 1
         else:
-            # Page hit → increase frequency
+           
             frequency[page] += 1
 
         print(f"{page}\t{cache}\t\t{'Yes' if fault else 'No'}")
